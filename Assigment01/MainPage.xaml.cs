58a1df3636cbf11441c5725b8747b06806b01d95
Antti Karjakin <antti.karjakin@gmail.com>
2013-09-15 18:30:53 +0300

    All controls are now on the app.
    Added controls. Next coming localization. Rest will be just fine tuning.

--- Assigment01/MainPage.xaml.cs
+++ Assigment01/MainPage.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
 namespace Assigment01
@@ -26,7 +18,11 @@ namespace Assigment01
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
             RadioButtonPublicPhone.IsChecked = true;
+            hyperlinkButtonSupportedBrowsers.Visibility = Visibility.Collapsed;
+
             RadioButtonPrivatePhone.Checked += RadioButtonPrivatePhoneOnChecked;
+            RadioButtonPublicPhone.Checked += RadioButtonPublicPhoneOnChecked;
+            CheckBoxUseLightVersion.Unchecked += CheckBoxUseLightVersionOnUnchecked;
 
             TextBlockPublicProtectedInfo.Text = "Valitse tämä vaihtoehto, jos käytät Outlook Web Appiä julkisessa tietokoneessa. Kun olet lopettanut, muista kirjautua ulos ja sulkea kaikki ikkunat istuntosi päätteeksi.";
             TextBlockPublicProtectedInfo.Visibility = Visibility.Collapsed;
@@ -41,6 +37,17 @@ namespace Assigment01
             TextBlockSecurityWarning.Visibility = Visibility.Collapsed;
         }
 
+        private void CheckBoxUseLightVersionOnUnchecked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            TextBlockLightVersionInfo.Visibility = Visibility.Collapsed;
+            hyperlinkButtonSupportedBrowsers.Visibility = Visibility.Collapsed;
+        }
+
+        private void RadioButtonPublicPhoneOnChecked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            TextBlockSecurityWarning.Visibility = Visibility.Collapsed;
+        }
+
         private void RadioButtonPrivatePhoneOnChecked(object sender, RoutedEventArgs routedEventArgs)
         {
             TextBlockSecurityWarning.Visibility = Visibility.Visible;
@@ -52,9 +59,31 @@ namespace Assigment01
                                                           ? Visibility.Visible
                                                           : Visibility.Collapsed;
 
+            //we define text from above textbox visibility ^^
+            TextBlockShowPublicPrivateInfo.Text = TextBlockPublicProtectedInfo.Visibility == Visibility.Collapsed
+                                                      ? "näytä selitys"
+                                                      : "piilota selitys";
+
             TextBlockPrivateProtectedInfo.Visibility = TextBlockPrivateProtectedInfo.Visibility == Visibility.Collapsed
                                                           ? Visibility.Visible
                                                           : Visibility.Collapsed;
+        }
+
+        private void CheckBoxUseLightVersion_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBlockLightVersionInfo.Visibility = Visibility.Visible;
+            hyperlinkButtonSupportedBrowsers.Visibility = Visibility.Visible;
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            String messageToUser = "login done";
+            if (String.IsNullOrEmpty(TextBoxUserName.Text) && String.IsNullOrEmpty(passwordBox1.Password))
+            {
+                 messageToUser = "invalid username and password";
+            }
+
+            MessageBox.Show(messageToUser);
         }
     }
 }
