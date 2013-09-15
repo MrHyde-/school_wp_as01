d02a98503b9d7ab15cf27281efc1222f41fef6c2
Antti Karjakin <antti.karjakin@gmail.com>
2013-09-15 19:31:36 +0300

    Started localizing the app..

---
+++ Assigment01/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Assigment01.Resources;
+
+namespace Assigment01
+{
+    public class LocalizedStrings
+    {
+        private static AppResources localizedResources = new AppResources();
+
+        public AppResources AppResources
+        {
+            get { return localizedResources; }
+        }
+    }
+}
--- Assigment01/MainPage.xaml.cs
+++ Assigment01/MainPage.xaml.cs
@@ -24,16 +24,13 @@ namespace Assigment01
             RadioButtonPublicPhone.Checked += RadioButtonPublicPhoneOnChecked;
             CheckBoxUseLightVersion.Unchecked += CheckBoxUseLightVersionOnUnchecked;
 
-            TextBlockPublicProtectedInfo.Text = "Valitse tämä vaihtoehto, jos käytät Outlook Web Appiä julkisessa tietokoneessa. Kun olet lopettanut, muista kirjautua ulos ja sulkea kaikki ikkunat istuntosi päätteeksi.";
             TextBlockPublicProtectedInfo.Visibility = Visibility.Collapsed;
 
-            TextBlockPrivateProtectedInfo.Text = "Valitse tämä asetus, jos olet tämän tietokoneen ainoa käyttäjä. Palvelimesi sallii tavallista pidemmän käyttämättömyysajan, ennen kuin sinut kirjataan ulos.";
             TextBlockPrivateProtectedInfo.Visibility = Visibility.Collapsed;
 
             TextBlockLightVersionInfo.Text = "Outlook Web Appin kevytversio sisältää vähemmän toimintoja. Käytä sitä, jos yhteytesi on hidas tai jos käytät tietokonetta, jonka selaimen suojausasetukset ovat tavallista rajoittavammat. Outlook-verkkosovelluksen täyttä versiota tuetaan myös joissakin Windows- ja Mac-tietokoneiden selaimissa. Jos haluat tarkistaa kaikki tuetut selaimet ja käyttöjärjestelmät, ";
             TextBlockLightVersionInfo.Visibility = Visibility.Collapsed;
 
-            TextBlockSecurityWarning.Text = "Varoitus: Valitsemalla tämän asetuksen vahvistat, että tämä tietokone on organisaatiosi tietoturvakäytännön mukainen.";
             TextBlockSecurityWarning.Visibility = Visibility.Collapsed;
         }
 
